4be2d5a27eac55caa6dd091da0687b500de6189c
hadasbgu <hadasganim@gmail.com>
2018-02-05 17:08:35 +0200

    deleted

--- script5.js
+++ script5.js
@@ -1,15 +1,19 @@
 // NodeJS Tutorial 5: Creating Simple Server - Part 2
 
-
 var http = require('http');
+var server = http.createServer(engine);
 
+// everytime the server recives a request the function happens
+server.listen(1337, function(){
+		console.log('server was hit by a request');
 
+}); 
 
-server.createServer(engine).listen(1337); // leets
 
 function engine(request, response){
 
+	// console.log(response)
 	response.writeHead(200, {'Content-Type': 'text/plain', 'blablabla': 'cool man'});
-	response.end('Hey, I am server :D ');
+	response.end('Hey, I am server :D request from page: ' + request.url);
 	
 }
